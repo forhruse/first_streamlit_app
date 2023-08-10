b72cedd66154fae1187c54cfeb2ea94848715d63
forhruse <141965785+forhruse@users.noreply.github.com>
2023-08-10 17:10:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 
-streamlit.title('Hi')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinich & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-range Egge')
 
 
