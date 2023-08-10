82a9290a90e6ee4144a6f4ed06b9c6998737cb67
forhruse <141965785+forhruse@users.noreply.github.com>
2023-08-10 17:14:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale, Spinich & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-range Egge')
-
+streamlit.title(' My Parents New Healthy Diner')
+streamlit.header(' Breakfast Favorites')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinich & Rocket Smoothie')
+streamlit.text(🐔 'Hard-Boiled Free-range Egge')
+streamlit.text(🥑🍞 'Avocado Toast')
 
