aa5ea516aff60d5af8609b638a8bef38b4e1b1f2
forhruse <141965785+forhruse@users.noreply.github.com>
2023-08-10 18:18:12 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,29 @@
 import streamlit as st
-
-st.title("My Mom's New Healthy Diner")
-st.header(' Breakfast Favorites')
-st.text('🥣 Omega 3 and Blueberry Oatmeal')
-st.text('🥗 Kale, Spinich & Rocket Smoothie')
-st.text('🐔 Hard-Boiled Free-range Egge')
-st.text('🥑🍞 Avocado Toast')
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 import pandas as pd
 
+# Title and header for the page
+st.title("My Mom's New Healthy Diner")
+st.header('Breakfast Favorites')
+st.text('🥣 Omega 3 and Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-range Egg')
+st.text('🥑🍞 Avocado Toast')
+
+# Header for the smoothie section
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# Load and process data
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
+# Let users pick the fruits for their smoothie
 selected_fruits = st.multiselect("Pick some fruits:", options=list(my_fruit_list.index), default=["Avocado", "Strawberries"])
 
 # Filter the dataframe based on the selected fruits
 filtered_fruit_list = my_fruit_list.loc[selected_fruits]
 
-# Display the table on the page.
-st.dataframe(my_fruit_list)
-
-
+# Display the filtered fruit data
+st.dataframe(filtered_fruit_list)
 
 
 
