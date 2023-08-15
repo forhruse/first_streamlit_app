3d31794a8bb3c0cf0b0a687c89b50f443c3ff38e
forhruse <141965785+forhruse@users.noreply.github.com>
2023-08-15 12:14:53 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,10 +45,12 @@ my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
+#my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 # st.text("Hello from Snowflake:")
-st.text("The fruit load list contains:")
-st.text(my_data_row)
+st.header("The fruit load list contains:")
+# st.text(my_data_row)
+st.dataframe(my_data_rows)
 
 
 
